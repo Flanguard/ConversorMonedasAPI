ceac1a62cd5b9ab40a514a89d8f85cc0b4c955ac
Flanguard <reyesbetancesamaury@outlook.es>
2024-06-03 17:19:59 -0400

    Agregando el HttpClient

---
+++ src/Conexion/ConectarAPI.java
@@ -0,0 +1,5 @@
+package Conexion;
+
+public class ConectarAPI {
+    String guardarApi = "GET https://v6.exchangerate-api.com/v6/6af216db4bf551cfe47c9c94/latest/USD";
+}
---
+++ src/Conexion/MisMonedas.java
@@ -0,0 +1,2 @@
+package Conexion;public record MisMonedas() {
+}
