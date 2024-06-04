2ae4f0fe5d87f5b866f681b44bf9da4cc1059fcc
Flanguard <reyesbetancesamaury@outlook.es>
2024-06-03 23:19:46 -0400

    Conversor finalizado Exitosamente

--- src/LanzarAplicacion/Principal.java
+++ src/LanzarAplicacion/Principal.java
@@ -6,28 +6,61 @@ import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.text.DecimalFormat;
 import java.util.Map;
 import java.util.Scanner;
 
 public class Principal {
     public static void main(String[] args) {
-        Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE).setPrettyPrinting().create();
-
         Scanner lectura = new Scanner(System.in);
+        DecimalFormat format = new DecimalFormat("#.##");
+        boolean trueFalse = true;
+        double conversion;
 
         ConectarAPI probando = new ConectarAPI();
-
-        Map<String, Double> monedas = probando.obtenerApi("USD").conversion_rates();
-
-        System.out.println(monedas.get("EUR"));
+        Map<String, Double> monedas;
         System.out.println("Bienvenido al conversor de monedas");
-        while(true){
+        while(trueFalse){
             System.out.println("""
                     **********************************
-                    
+                    1) Peso Dominicano a Dolar Estadounidense
+                    2) Dolar Estadounidense a Peso Dominicano
+                    3) Peso Dominicano a Euro
+                    4) Euro a Peso Dominicano
+                    5) Salir
+                    **********************************
                     """);
+            System.out.println("Cual opcion elige?");
+            int opcion = lectura.nextInt();
+            switch (opcion){
+                case 1:
+                    System.out.print("Que valor desea convertir: ");
+                    conversion = lectura.nextDouble();
+                    monedas =  probando.obtenerApi("DOP").conversion_rates();
+                    System.out.println("El equivalente en dolares es: " + format.format(conversion * monedas.get("USD"))+" USD");
+                    break;
+                case 2:
+                    System.out.print("Ingrese un valor: ");
+                    conversion = lectura.nextDouble();
+                    monedas =  probando.obtenerApi("USD").conversion_rates();
+                    System.out.println(monedas.get("USD"));
+                    System.out.println("El equivalente en pesos dominicanos es: " + format.format(conversion * monedas.get("DOP"))+" DOP");
+                    break;
+                case 3:
+                    System.out.print("Ingrese un valor: ");
+                    conversion = lectura.nextDouble();
+                    monedas =  probando.obtenerApi("DOP").conversion_rates();
+                    System.out.println("El equivalente en Euros es: " + format.format(conversion * monedas.get("EUR"))+" EUR");
+                    break;
+                case 4:
+                    System.out.print("Ingrese un valor: ");
+                    conversion = lectura.nextDouble();
+                    monedas =  probando.obtenerApi("EUR").conversion_rates();
+                    System.out.println("El equivalente en pesos dominicanos es: " + format.format(conversion * monedas.get("DOP"))+" DOP");
+                    break;
+                default:
+                    trueFalse = false;
+            }
         }
-
-
     }
 }
